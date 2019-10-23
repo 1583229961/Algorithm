bfa8af2c5e271f12ea23de4d7513d2be853d32bc
1583229961 <49240654+1583229961@users.noreply.github.com>
2019-10-23 22:42:02 +0800

    Algorithm

---
+++ src/i/algorithm/dynamic/CutRope.java
@@ -0,0 +1,29 @@
+package i.algorithm.dynamic;
+
+import java.util.Scanner;
+
+public class CutRope {
+	public static void rope(int a) {
+		int []dp=new int[a+1];
+		dp[0]=0;
+		dp[1]=1;
+		dp[2]=2;
+		dp[3]=3;
+		for (int i = 4; i < a+1; i++) {
+			for (int j = 1; j <= i/2; j++) {//取j=i/2是为了避免重复的运算.如2*6和6*2相同
+				dp[i]=Math.max(dp[j]*dp[i-j],dp[i]);
+			}
+			
+		}
+		
+
+		System.out.println(dp[a]);
+	}
+	
+	
+public static void main(String []args) {
+	Scanner scanner=new Scanner(System.in);
+	int a=scanner.nextInt();
+	rope(a);
+}
+}
